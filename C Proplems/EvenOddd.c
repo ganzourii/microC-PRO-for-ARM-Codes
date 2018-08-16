2a921cd19c20709cfa33ac4aee1490ccfce02307
Ahmad Ganzouri <ganzouri97@gmail.com>
2018-08-16 22:38:10 +0200

    Add files via upload

---
+++ C Proplems/EvenOddd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main (void){
+	int x;
+
+	
+	printf("Enter Number \n");
+	scanf ("%d",&x);
+	if ( x%2 ==0) {	printf("Number is even \n"); }
+	else {	printf("Number is odd \n");}
+	
+    
+}
